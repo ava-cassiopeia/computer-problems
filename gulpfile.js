e0a702e2af4881fd36ebc9c868b8097ee13296db
Ivan Mattie <mattieivan@gmail.com>
2017-01-31 21:42:56 -0700

    Added build process for building uglified JS, closes #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
 
-gulp.task("default", ["build-css"], function() {});
+gulp.task("default", ["build-css", "build-js"], function() {});
 
 gulp.task("build-css", function() {
     return gulp.src(["sass/*.scss", "sass/**/*.scss"])
         .pipe(sass())
         .pipe(gulp.dest("css/"));
+});
+
+gulp.task("build-js", function() {
+    return gulp.src("app.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("js/"));
 });
 
 gulp.task("watch", function() {
